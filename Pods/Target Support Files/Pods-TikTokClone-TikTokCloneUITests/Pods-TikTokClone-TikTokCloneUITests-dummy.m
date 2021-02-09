cbc5e6f80366d8a24efdb206f142a7f8a7d7df2e
nicolas sieiro <n.sieiro42@gmail.com>
2021-02-09 05:21:01 -0300

    remove Pods

--- Pods/Target Support Files/Pods-TikTokClone-TikTokCloneUITests/Pods-TikTokClone-TikTokCloneUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TikTokClone_TikTokCloneUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TikTokClone_TikTokCloneUITests
-@end
--- Pods/Target Support Files/Pods-TikTokClone/Pods-TikTokClone-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TikTokClone : NSObject
-@end
-@implementation PodsDummy_Pods_TikTokClone
-@end
